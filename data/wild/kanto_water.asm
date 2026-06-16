c71f175ff9989685b933581481b66a8e6854b0ec
cmartyniuk <cmartyniuk1@gmail.com>
2026-06-15 21:19:21 -0400

    Overhaul wild Pokémon encounters and refine treemon mechanics
    *   Redefines wild Pokémon spawns and levels across all Johto and Kanto grass and water maps.
    *   Removes version-specific (Gold/Silver) wild data, unifying the encounter tables.
    *   Expands treemon sets with new categories (Town, Route, Kanto, Lake) and explicitly separates Rock Smash encounters for better management.
    *   Corrects a minor fishing group classification in Union Cave B2F.

--- data/wild/kanto_water.asm
+++ data/wild/kanto_water.asm
@@ -2,6 +2,20 @@
 
 KantoWaterWildMons:
 
+	def_water_wildmons TOHJO_FALLS
+	db 4 percent ; encounter rate
+	db 20, GOLDEEN
+	db 20, SLOWPOKE
+	db 20, SEAKING
+	end_water_wildmons
+
+	def_water_wildmons VERMILION_PORT
+	db 2 percent ; encounter rate
+	db 35, TENTACOOL
+	db 30, TENTACOOL
+	db 35, TENTACRUEL
+	end_water_wildmons
+
 	def_water_wildmons ROUTE_4
 	db 4 percent ; encounter rate
 	db 10, GOLDEEN
@@ -100,13 +114,6 @@ KantoWaterWildMons:
 	db 20, TENTACRUEL
 	end_water_wildmons
 
-	def_water_wildmons TOHJO_FALLS
-	db 4 percent ; encounter rate
-	db 20, GOLDEEN
-	db 20, SLOWPOKE
-	db 20, SEAKING
-	end_water_wildmons
-
 	def_water_wildmons ROUTE_28
 	db 2 percent ; encounter rate
 	db 40, POLIWAG
@@ -158,13 +165,6 @@ KantoWaterWildMons:
 
 	def_water_wildmons CINNABAR_ISLAND
 	db 6 percent ; encounter rate
-	db 35, TENTACOOL
-	db 30, TENTACOOL
-	db 35, TENTACRUEL
-	end_water_wildmons
-
-	def_water_wildmons VERMILION_PORT
-	db 2 percent ; encounter rate
 	db 35, TENTACOOL
 	db 30, TENTACOOL
 	db 35, TENTACRUEL
